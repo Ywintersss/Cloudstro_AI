a7bd179b393b822c7eee6d8c8480c798cf95d8e5
Ywintersss <shawnpvp@gmail.com>
2025-09-23 02:30:28 +0800

    test

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -8,6 +8,12 @@ export async function GET(request: NextRequest) {
     console.log(process.env.MY_SECRET_ACCESS_KEY! as string);
     console.log(process.env.BEDROCK_ARN! as string);
 
+    return NextResponse.json({
+      success: true,
+      message: "API working perfectly!",
+      body: process.env.TEST_VAR
+    });
+
   } catch (error) {
     console.error('Error fetching user:', error);
     return NextResponse.json(
